b913da233733e4b108a11244613137f17fac0a59
Tony <Tony@Tony>
2025-04-24 19:58:24 +0200

    test

--- cats/urls.py
+++ cats/urls.py
@@ -12,9 +12,9 @@ router.register(r'tags', views.TagViewSet)
 router.register(r'foto-tags', views.FotoTagViewSet)
 router.register(r'badges', views.BadgeViewSet)
 router.register(r'gato-badges', views.GatoBadgeViewSet)
-router.register(r'chats', views.ChatViewSet)
-router.register(r'mensajes', views.MensajeViewSet)
-router.register(r'notificaciones', views.NotificacionViewSet)
+router.register(r'chats', views.ChatViewSet, basename='chat')
+router.register(r'mensajes', views.MensajeViewSet, basename='mensaje')
+router.register(r'notificaciones', views.NotificacionViewSet, basename='notificacion')
 
 urlpatterns = [
     path('', include(router.urls)),
